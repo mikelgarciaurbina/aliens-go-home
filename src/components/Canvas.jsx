45236029cb0806b78a0704cec067aa73252fde04
Mikel Garcia <mikel_6@hotmail.com>
2018-06-01 11:09:09 +0200

    Make Canvas dynamically with angle and trackMouse props

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
 
-const Canvas = () => {
+const Canvas = ({ angle, trackMouse }) => {
   const viewBox = [
     window.innerWidth / -2,
     100 - window.innerHeight,
@@ -15,15 +16,20 @@ const Canvas = () => {
   return (
     <svg
       id="aliens-go-home-canvas"
+      onMouseMove={trackMouse}
       preserveAspectRatio="xMaxYMax none"
       viewBox={viewBox}
     >
       <Sky />
       <Ground />
-      <CannonPipe rotation={45} />
+      <CannonPipe rotation={angle} />
       <CannonBase />
     </svg>
   );
+};
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
 };
 
 export default Canvas;
