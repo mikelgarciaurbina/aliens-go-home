e5ab1bd5b8ae78223dd69de13354fdff634ab790
Mikel Garcia <mikel_6@hotmail.com>
2018-06-02 09:40:55 +0200

    Add CurrentScore component to Canvas and new filter shadow

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import CannonBall from './CannonBall';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CurrentScore from './CurrentScore';
 import Ground from './Ground';
 import Sky from './Sky';
 
@@ -22,14 +23,21 @@ const Canvas = ({ angle, trackMouse }) => {
       preserveAspectRatio="xMaxYMax none"
       viewBox={viewBox}
     >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
       <Sky />
       <Ground />
       <CannonPipe rotation={angle} />
       <CannonBase />
       <CannonBall position={{ x: 0, y: -100 }} />
+      <CurrentScore score={15} />
     </svg>
   );
 };
+
 Canvas.propTypes = {
   angle: PropTypes.number.isRequired,
   trackMouse: PropTypes.func.isRequired,
