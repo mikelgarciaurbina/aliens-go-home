96f8eb1391f121998c742e0f23e2c18931f56e05
Mikel Garcia <mikel_6@hotmail.com>
2018-06-02 10:48:25 +0200

    Add new constants for flyingObjects

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
+export const createInterval = 1000;
+
+export const maxFlyingObjects = 4;
+
+export const flyingObjectsStarterYAxis = -1000;
+
+export const flyingObjectsStarterPositions = [-300, -150, 150, 300];
+
 export const gameWidth = 800;
+
 export const skyAndGroundWidth = 5000;
