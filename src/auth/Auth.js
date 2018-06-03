f5ff6498ca2de3dc48e4c22aa25c4d526e7f9cde
Mikel Garcia <mikel_6@hotmail.com>
2018-06-03 19:26:36 +0200

    Add session methods in Auth

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -11,16 +11,44 @@ export default class Auth {
       scope: 'openid profile manage:points',
     });
     this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
     this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   handleAuthentication() {
     this.auth0.parseHash((err, authResult) => {
-      console.log(authResult);
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+      } else if (err) {
+        console.log(err);
+      }
     });
+  }
+
+  isAuthenticated() {
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+
+    return new Date().getTime() < expiresAt;
   }
 
   login() {
     this.auth0.authorize();
+  }
+
+  logout() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+  }
+
+  setSession(authResult) {
+    let expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime(),
+    );
+
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
   }
 }
