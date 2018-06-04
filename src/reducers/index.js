c365e3843ae873181f8a279a5ab79de2af5b4078
Mikel Garcia <mikel_6@hotmail.com>
2018-06-04 09:43:10 +0200

    Add leaderboardLoaded and loggedIn reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,19 @@
-import { MOVE_OBJECTS, START_GAME } from '../actions';
+import {
+  LEADERBOARD_LOADED,
+  LOGGED_IN,
+  MOVE_OBJECTS,
+  START_GAME,
+} from '../actions';
 import moveObjects from './moveObjects';
 import startGame from './startGame';
 
 const initialGameState = {
+  currentPlayer: null,
   flyingObjects: [],
   kills: 0,
   lastObjectCreatedAt: new Date(),
   lives: 3,
+  players: null,
   started: false,
 };
 
@@ -17,6 +24,16 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case LEADERBOARD_LOADED:
+      return {
+        ...state,
+        players: action.players,
+      };
+    case LOGGED_IN:
+      return {
+        ...state,
+        currentPlayer: action.player,
+      };
     case MOVE_OBJECTS:
       return moveObjects(state, action);
     case START_GAME:
