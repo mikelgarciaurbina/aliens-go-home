2d4d6c42fa432c78b93c82bf49b73b136c6f1980
Mikel Garcia <mikel_6@hotmail.com>
2018-06-04 09:26:10 +0200

    Change Login for Leaderboard in Canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -8,7 +8,7 @@ import CurrentScore from './CurrentScore';
 import FlyingObject from './FlyingObject';
 import Ground from './Ground';
 import Heart from './Heart';
-import Login from './Login';
+import Leaderboard from './Leaderboard';
 import Sky from './Sky';
 import StartGame from './StartGame';
 import Title from './Title';
@@ -20,6 +20,56 @@ const Canvas = ({ angle, auth, gameState, startGame, trackMouse }) => {
     100 - gameHeight,
     window.innerWidth,
     gameHeight,
+  ];
+  const leaderboard = [
+    {
+      id: 'd4',
+      maxScore: 82,
+      name: 'Ado Kukic',
+      picture: 'https://twitter.com/KukicAdo/profile_image',
+    },
+    {
+      id: 'a1',
+      maxScore: 235,
+      name: 'Bruno Krebs',
+      picture: 'https://twitter.com/brunoskrebs/profile_image',
+    },
+    {
+      id: 'c3',
+      maxScore: 99,
+      name: 'Diego Poza',
+      picture: 'https://twitter.com/diegopoza/profile_image',
+    },
+    {
+      id: 'b2',
+      maxScore: 129,
+      name: 'Jeana Tahnk',
+      picture: 'https://twitter.com/jeanatahnk/profile_image',
+    },
+    {
+      id: 'e5',
+      maxScore: 34,
+      name: 'Jenny Obrien',
+      picture: 'https://twitter.com/jenny_obrien/profile_image',
+    },
+    {
+      id: 'f6',
+      maxScore: 153,
+      name: 'Kim Maida',
+      picture: 'https://twitter.com/KimMaida/profile_image',
+    },
+    {
+      id: 'g7',
+      maxScore: 55,
+      name: 'Luke Oliff',
+      picture: 'https://twitter.com/mroliff/profile_image',
+    },
+    {
+      id: 'h8',
+      maxScore: 146,
+      name: 'Sebastián Peyrott',
+      picture: 'https://twitter.com/speyrott/profile_image',
+    },
   ];
 
   return (
@@ -44,7 +94,7 @@ const Canvas = ({ angle, auth, gameState, startGame, trackMouse }) => {
         <g>
           <StartGame onClick={() => startGame()} />
           <Title />
-          <Login auth={auth} />
+          <Leaderboard auth={auth} currentPlayer={leaderboard[6]} leaderboard={leaderboard} />
         </g>
       )}
       {gameState.started &&
