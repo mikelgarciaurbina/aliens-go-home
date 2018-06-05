252a5b4c0662f6f45efb0b8f81156d5b7a36efef
Mikel Garcia <mikel_6@hotmail.com>
2018-06-05 09:19:17 +0200

    Add losLife when flyingObjects arrive to the earth

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -15,21 +15,41 @@ function moveObjects(state, action) {
 
   const newState = createFlyingObjects(state);
 
-  const now = (new Date()).getTime();
-  let flyingObjects = newState.gameState.flyingObjects.filter(object => (
-    (now - object.createdAt) < 4000
-  ));
+  const now = new Date().getTime();
+  let flyingObjects = newState.gameState.flyingObjects.filter(
+    object => now - object.createdAt < 4000,
+  );
+
+  const lostLife = state.gameState.flyingObjects.length > flyingObjects.length;
+  let lives = state.gameState.lives;
+  if (lostLife) {
+    lives--;
+  }
+
+  const started = lives > 0;
+  if (!started) {
+    flyingObjects = [];
+    cannonBalls = [];
+    lives = 3;
+  }
 
   const { x, y } = mousePosition;
   const angle = calculateAngle(0, 0, x, y);
 
   const objectsDestroyed = checkCollisions(cannonBalls, flyingObjects);
-  const cannonBallsDestroyed = objectsDestroyed.map(object => (object.cannonBallId));
-  const flyingDiscsDestroyed = objectsDestroyed.map(object => (object.flyingDiscId));
+  const cannonBallsDestroyed = objectsDestroyed.map(
+    object => object.cannonBallId,
+  );
+  const flyingDiscsDestroyed = objectsDestroyed.map(
+    object => object.flyingDiscId,
+  );
 
-  cannonBalls = cannonBalls.filter(cannonBall => (cannonBallsDestroyed.indexOf(cannonBall.id)));
-  flyingObjects = flyingObjects.filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)));
-
+  cannonBalls = cannonBalls.filter(cannonBall =>
+    cannonBallsDestroyed.indexOf(cannonBall.id),
+  );
+  flyingObjects = flyingObjects.filter(flyingDisc =>
+    flyingDiscsDestroyed.indexOf(flyingDisc.id),
+  );
 
   return {
     ...newState,
@@ -38,6 +58,8 @@ function moveObjects(state, action) {
       ...newState.gameState,
       cannonBalls,
       flyingObjects,
+      lives,
+      started,
     },
   };
 }
